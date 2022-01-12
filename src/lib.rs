92f426446ac342a398dcb68ed444292ed1b343ad
Shisei Hanai <shanai@jp.ibm.com>
2022-01-12 15:17:26 +0900

    Create module.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
+pub mod starter_hello_world {
+    const MESSAGE: &str = "Hello, world!";
+
+    pub fn greet() {
+        println!("{}", MESSAGE);
+    }
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn check_message() {
+        assert_eq!(MESSAGE, "Hello, world!");
     }
 }
